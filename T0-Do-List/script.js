05a4c4c65aa8854823ed9f5eb3a96303dd7ab035
Raahul423 <rpal24480@gmail.com>
2024-11-22 14:58:49 +0530

    files

--- T0-Do-List/script.js
+++ T0-Do-List/script.js
@@ -5,18 +5,20 @@ function addtask(){
     if(input.value ==''){
         alert("Please Enter Something!")
     }else{
-        let li=document.createElement('li')
+        let li=document.createElement("li")
         li.innerHTML=input.value
         unorderlist.appendChild(li);
+        let span=document.createElement("span")
+        span.innerHTML="\u00d7"
+        li.appendChild(span)
     }
     input.value=''
 }
 
 unorderlist.addEventListener('click',(e)=>{
     if(e.target.tagName =="LI"){
-        console.log("hii")
-        // e.target.classList.add("checked")
-    }else{
-        console.log("hello")
+        e.target.classList.add("checked")
+    }else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
     }
-});
+},false);
